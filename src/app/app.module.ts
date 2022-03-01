8320890a780736a202cb7fd670188c21211893d2
Sasha-N <avgystina17@mail.ru>
2022-03-01 20:36:39 +0700

    organizer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MomentPipe } from './shared/moment.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent,
+    MomentPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/organizer/organizer.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DateService } from '../shared/date.service';
+
+@Component({
+  selector: 'app-organizer',
+  templateUrl: './organizer.component.html',
+  styleUrls: ['./organizer.component.scss']
+})
+export class OrganizerComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(public dateService: DateService) { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required)
+    }
+
+    )
+  }
+
+  submit() {
+    const {title} = this.form.controls
+    console.log(title)
+  }
+
+}
